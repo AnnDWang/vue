634841543ff80a8a14cb59ce7ba302222017bc14
DanGoing <2506167296@qq.com>
2017-05-13 16:04:47 +0800

    update html add css and js

---
+++ markdown/js/markdown.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:'#editor',
+    data:{
+        input:'#hello'
+    },
+    computed:{
+        compiledMarkdown:function(){
+            return marked(this.input,{sanitize:true})
+        }
+    },
+    methods:{
+         update:function (e) {
+      this.input = e.target.value
+    }
+    }
+})
